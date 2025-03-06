d5a9d7b62dc59991adfd6cc6448b1c6367c246e3
Moritz von Pokrzywnicki <mvp@q-con.eu>
2025-03-06 13:41:43 +0000

    copyright adjusted

--- app/app/App/Http/Controllers/CopyrightController.php
+++ app/app/App/Http/Controllers/CopyrightController.php
@@ -16,11 +16,7 @@ class CopyrightController extends Controller
 
     // Generated with  https://patorjk.com/software/taag/
     public const COPYRIGHT = "
- ___       _   _  _ _  ___  _  _  __  _   _  ___  __  _   _     __          _ _
-| o )__   | \_/ || | || __|| \| |/ _|| \_/ || __||  \| | / \   / _|  _ _ ||| U |
-| o \\ V7 | \_/ || U || _| | \\ |\_ \| \_/ || _| | o ) || o | ( |_n|/ \ \|o\   |
-|___/ )/  |_| |_||___||___||_|\_||__/|_| |_||___||__/|_||_n_|  \__/L_n_n||_/_n_|
-     //
+    by Moritz von Pokrzywnicki :)
         ";
 
     static public function getCopyright(): string
